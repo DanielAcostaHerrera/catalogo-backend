c91701cde6b1573b042cf0d81b9057d6c5604279
Daniel <danieldavidacostaherrera@gmail.com>
2026-01-26 03:47:51 -0500

    Eliminada referencia a carpeta portadas en render

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,11 +10,6 @@ async function bootstrap() {
     origin: '*', // o pon aquí la URL de tu frontend si quieres restringir
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-  });
-
-  // Servir carpeta de portadas (funciona en local y en Render)
-  app.useStaticAssets(join(__dirname, '..', 'portadas'), {
-    prefix: '/portadas/',
   });
 
   // Usar el puerto asignado por Render o 3000 en local
